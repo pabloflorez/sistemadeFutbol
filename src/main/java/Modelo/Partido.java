228f27ad039acfcbb44f3b7b96ab2b121625928d
pabloflorez <66337036+pabloflorez@users.noreply.github.com>
2020-06-07 19:14:12 -0500

    Update Partido.java

--- src/main/java/Modelo/Partido.java
+++ src/main/java/Modelo/Partido.java
@@ -1,18 +1,27 @@
+package Modelo;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Modelo;
 
 /**
- *
- * @author Pablo
+ * esta clase se encarga de crear los partidos del torneo
+ * 26/05/2020
+ * @author Juan y Jeny
  */
+
 public class Partido{
     
     Equipo equipo1;
     Equipo equipo2;
+    
+    /**
+     * este constructor recibe de a 2 equipos para enfrentarlos en un partido
+     * @param e1 es el equipo uno
+     * @param e2 es el equipo dos
+     */
     
     public Partido(Equipo e1, Equipo e2){
         equipo1 = e1;
